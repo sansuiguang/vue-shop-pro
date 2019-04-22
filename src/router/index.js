d46a5c81864640096f70d15ab486bebb50f1855a
sansuiguang <326121753@qq.com>
2019-04-22 11:36:43 +0800

    修改好的login

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,17 @@ import Login from '@/components/Login'
 import Home from '@/components/Home'
 Vue.use(Router)
 const router = new Router({
-  routes:[
-    {path:'/login',component:Login},
-    {path:'home',component:Home}
+  routes: [
+    {path: '/login', component: Login},
+    {path: 'home', component: Home}
   ]
 })
 // 设置路由守卫
-router.beforeEach((to,from,naxt) => {
-  // 当前用户没有登录系统，并且还在访问非登录路由，就强制执行登录路由
+
+router.beforeEach((to, from, next) => {
+  // home/路由需要token的存在支持(表示用户处于登录状态)
   var token = window.sessionStorage.getItem('token')
-  if (!token && to.path!=='/login') {
+  if (!token && to.path !== '/login') {
     return next('/login')
   }
   next() // 请继续
